bc39c319fb3e7ba4e752c7ef7426b183804aaa01
Naxden <kilarjan05@gmail.com>
2025-01-05 12:31:48 +0100

    [GameInstance] Pass loaded save beteween levels

---
+++ Source/RacingEngineer/RacingEngineerGameInstance.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RacingEngineerGameInstance.generated.h"
+
+class URacingEngineerSaveGame;
+/**
+ * 
+ */
+UCLASS()
+class RACINGENGINEER_API URacingEngineerGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameInstance")
+	TObjectPtr<UTexture2D> SelectedMapTexture;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameInstance")
+	TObjectPtr<URacingEngineerSaveGame> SelectedSaveSlot;
+};
